b2d05299dc02297f7c3bcb9c19f9f9c6eb297c12
ckyy-beep <jackknife29jk@gmail.com>
2026-03-01 14:05:34 +0100

    course_update

--- asteroid.py
+++ asteroid.py
@@ -22,14 +22,17 @@ class Asteroid(CircleShape):
         random_angle = random.uniform(20, 50)
         new_velocity_one = pygame.math.Vector2.rotate(self.velocity, random_angle)
         new_velocity_two = pygame.math.Vector2.rotate(self.velocity, - random_angle)
+        # new_velocity_three = pygame.math.Vector2.rotate(self.velocity, - random_angle + 25)
 
         new_smaller_radius = self.radius - ASTEROID_MIN_RADIUS
 
         new_asteroid_one = Asteroid(self.position.x, self.position.y, new_smaller_radius)
         new_asteroid_two = Asteroid(self.position.x, self.position.y, new_smaller_radius)
+        # new_asteroid_three = Asteroid(self.position.x, self.position.y, new_smaller_radius)
 
         new_asteroid_one.velocity = new_velocity_one * 1.2
         new_asteroid_two.velocity = new_velocity_two * 1.2
+        #new_asteroid_three.velocity = new_velocity_three * 1.2
 
 
 
